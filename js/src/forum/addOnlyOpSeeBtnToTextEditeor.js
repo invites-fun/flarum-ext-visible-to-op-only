2972d2dadf4b13a788be0f02abfcad667260b84b
liufei <liufei@liufei.info>
2024-06-18 17:51:28 +0800

    修改 支持指定分区下开启进楼主可见权限

---
+++ js/src/forum/addOnlyOpSeeBtnToTextEditeor.js
@@ -0,0 +1,26 @@
+import {extend} from "flarum/common/extend";
+import TextEditor from "flarum/common/components/TextEditor";
+import TextEditorButton from "flarum/common/components/TextEditorButton";
+import styleSelectedText from 'flarum/common/utils/styleSelectedText';
+import {trans} from "../common";
+
+const style = {prefix: '[OP]', suffix: '[/OP]', trimFirst: true};
+
+export default function addOnlyOpSeeBtnToTextEditeor() {
+  extend(TextEditor.prototype, 'toolbarItems', function (items) {
+    if (!app.session.user.attribute('canVisibleToOpPermissionsViewButton')) {
+      return;
+    }
+    if (app.composer.body.attrs.discussion || app.composer.body.attrs.post) {
+      items.add(
+        "only-op-see",
+        <TextEditorButton
+          onclick={() => styleSelectedText(this.attrs.composer.editor.el, style)}
+          icon="fas fa-user-shield"
+        >
+          {trans("forum.button_tooltip_only_op_see")}
+        </TextEditorButton>
+      );
+    }
+  })
+}
---
+++ js/src/forum/renderOnlyOpSee.js
@@ -0,0 +1,38 @@
+import {override} from 'flarum/common/extend';
+import Post from 'flarum/common/models/Post';
+import {trans} from "../common";
+
+function onlyOpSee(innerHTML, canViewHidePosts) {
+  return innerHTML.replace(/<onlyopsee>(.*?)<\/onlyopsee>/gis, function (full, text) {
+    return render(canViewHidePosts, text);
+  })
+}
+
+function render(canViewHidePosts, text) {
+  let title = trans(canViewHidePosts ? `forum.only_op_see` : 'forum.hidden_content_only_op_see')
+  if (!app.session.user.attribute('canVisibleToOpPermissionsViewButton') && text) {
+    return `${text || ""}`
+  }
+  return `<div class="onlyopsee" data-title-content="${title}" >${text || ""}</div>`
+}
+
+export default function renderOnlyOpSee() {
+  override(Post.prototype, 'contentHtml', function (original, content) {
+
+    // 如果不能看见正文 就直接给拒绝文案
+    if (!this.attribute('canViewPosts')) {
+      return render(false)
+    }
+
+    // 否则替换正文
+    return onlyOpSee(original(), this.attribute('canViewHidePosts'))
+  });
+
+  // 预览时
+  override(s9e.TextFormatter, 'preview', (original, text, element) => {
+    original(text, element);
+
+    element.innerHTML = onlyOpSee(element.innerHTML, true)
+  });
+
+}
